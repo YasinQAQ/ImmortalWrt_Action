26de2277eb9a0b4bb749951abb5cad6c7a2f68fe
Yasin Chan <yasin@192.168.2.101>
2022-05-08 21:11:46 +0800

    test

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Add a feed source
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
